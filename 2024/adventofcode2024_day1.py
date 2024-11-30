fdaf67c3200382e5b1be546a3c912ca74cf7a119
troutnodoubt <truta11@yahoo.com>
2024-11-30 11:52:47 -0600

    day1 placeholder

---
+++ 2024/adventofcode2024_day1.py
@@ -0,0 +1,7 @@
+fname='input_day1.txt'
+fname='example_day1.txt'
+with open(fname) as fp: data = fp.read().splitlines()
+
+            
+#print('Part 1 solution', total)
+#print('Part 2 solution', total2)
