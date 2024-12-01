1b77860260da1fbe4a79276e390e990cd85435c2
troutnodoubt <truta11@yahoo.com>
2024-11-30 23:16:00 -0600

    day1

--- 2024/adventofcode2024_day1.py
+++ 2024/adventofcode2024_day1.py
@@ -1,7 +1,22 @@
 fname='input_day1.txt'
-fname='example_day1.txt'
+#fname='example_day1.txt'
 with open(fname) as fp: data = fp.read().splitlines()
 
-            
-#print('Part 1 solution', total)
-#print('Part 2 solution', total2)
+left=[]
+right=[]
+
+for entry in data:
+    left.append(int(entry.split()[0]))
+    right.append(int(entry.split()[1]))
+
+left.sort()
+right.sort()
+
+total=0
+total2=0
+for i in range(len(left)):
+    total+=abs(left[i] - right[i])
+    total2+=left[i]*right.count(left[i])
+
+print('Part 1 solution', total)
+print('Part 2 solution', total2)
