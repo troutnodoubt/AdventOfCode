3919af1357787247b37bfccad3f06bd3f1110d49
troutnodoubt <truta11@yahoo.com>
2024-12-27 14:06:12 -0600

    day19 pt 1 working

--- 2024/adventofcode2024_day19.py
+++ 2024/adventofcode2024_day19.py
@@ -1,18 +1,23 @@
+from functools import lru_cache
 
-# fname='input_day19.txt'
+fname='input_day19.txt'
 
-# with open(fname) as fp: data = fp.read().splitlines()
+with open(fname) as fp: data = fp.read().splitlines()
 
-data=['r, wr, b, g, bwu, rb, gb, br',
-       '',
-       'brwrr',
-       'bggr',
-       'gbbr',
-       'rrbgbr',
-       'ubwu',
-       'bwurrg',
-       'brgr',
-       'bbrgwb']
+# data=['r, wr, b, g, bwu, rb, gb, br',
+#        '',
+#        'brwrr',
+#        'bggr',
+#        'gbbr',
+#        'rrbgbr',
+#        'ubwu',
+#        'bwurrg',
+#        'brgr',
+#        'bbrgwb']
+
+# data=['abcd, abc, def',
+#       '',
+#       'abcdef']
 
 towels=data[0].split(', ')
 data.pop(0)
@@ -22,56 +27,51 @@ for towel in towels:
     if len(towel)>maxsizetocheck: maxsizetocheck=len(towel)
 
 # print(towels)
-# print(maxsizetocheck)
+print(maxsizetocheck)
 
-def isValidPattern(pattern,towels):
-    idx=0
-    patternstring=pattern[idx]
-    matches=[]
-    
-    while len(patternstring)>0:
-        print(patternstring)
-        # print(idx)
-        # print(len(pattern))
-        print(matches)
+@lru_cache(maxsize=None)
+def isValidPattern(patterntomatch,towels,maxsize,debug=False):
+    size=min(maxsize,len(patterntomatch))
+    patterns=[patterntomatch[:size-reduction] for reduction in range(size)]
+    if debug:
         print()
-        if patternstring in towels and idx in range(len(pattern)):
-            
-            matches.append(patternstring)
-            if idx+1 in range(len(pattern)):
-                print('next char is', pattern[idx+1])
-                patternstring=pattern[idx+1]
-        else:
-            if idx+1 in range(len(pattern)): patternstring=patternstring+pattern[idx+1]
-            else: 
-                matchFound=False
-                break
-        idx+=1
-        if idx>len(pattern): break
+        print('patterns',patterns)
+    builtpattern=''
+    for pattern in patterns:
 
-        test=''.join(a for a in matches)
-        
-        if test==pattern:
-            matchFound=True
-            break
-    print(test)
-    print(pattern)  
-    print(matchFound)
-    return matchFound  
+        if pattern in towels:
+            newpatterntomatch=patterntomatch[len(pattern):]
+            if debug: 
+                print('before recursive call', pattern)
+                print('to match',newpatterntomatch)
+                print('built   ',builtpattern)
+                    
+            builtpattern=pattern+isValidPattern(newpatterntomatch,towels,maxsize,debug)
+            if debug:
+                print('after recursive call', pattern)
+                print('to match',newpatterntomatch)
+                print('built   ',builtpattern)
+            if builtpattern==pattern+newpatterntomatch: 
+                if debug: print('early return',builtpattern)
+                return builtpattern
+    if debug: print('return  ',builtpattern) 
+    return builtpattern
+    
+validpatterns=set()
+invalidpatterns=set()
+for i,pattern in enumerate(data):
+    a=isValidPattern(str(pattern),tuple(towels),maxsizetocheck)
+    if a==pattern: 
+        validpatterns.add(a)
 
-# isValidPattern('brwrr',towels)
+print('Part 1',len(validpatterns)) #296 is too low, but is valid answer for someone else, #301 is too low
 
-validcount=0
-for pattern in data:
-    print()
-    print()
-    print(pattern)
-    if isValidPattern(pattern,towels): validcount+=1
+# Looks like I might have outsmarted myself, maybe
 
-print(validcount)
-# pattern=brwrr
-# put first character on stack
-# stack = b
-# is stack in towels
-# if yes, pattern = rwrr and empty the stack, repeat with rest of the string
-# if no, put next character on stack
+# Might have been smarter to see if towel in towels was in the pattern, and then generate combinations from that, but maybe not. Would still be a bunch of combos.
+
+# For part 2, try returning the list of towels that were used, and then expand each of those into their smaller components.
+
+# Only thing that might not work is if there is overlap, like if there was abcd and efgh and cdef and gh and ab in towels, which should then give [abcd,efgh] and [ab,cedf,gh] as options
+
+
