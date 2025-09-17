a94a759d2ce6e5d545a0a7e21969a07ea162db2f
troutnodoubt <truta11@yahoo.com>
2025-09-16 21:20:29 -0500

    2015 day 10 complete

--- 2015/adventofcode2015_day10.c
+++ 2015/adventofcode2015_day10.c
@@ -2,80 +2,51 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-int newString(char* oldString, char* newString, int length){
+char* newString(char* old){
     int count=1;
-    int lenNextString=0;
-    const int asciioffset=48;
-    char currentChar=oldString[0];
-    char nextChar = '\0';
-    printf("lenght %d\n",length);
-    if (1 == length){
-        newString[0]=length+asciioffset;
-        newString[1]=currentChar;
-        lenNextString+=2;
-    }
-    else {    
-        for (int i=1; i<length; i++){
-            nextChar=oldString[i];
-            printf("current char %c nextChar %c\n",currentChar,nextChar);
-            if (nextChar != '1' || nextChar != '2' || nextChar != '3' ||
-                nextChar != '4' || nextChar != '5' || nextChar != '6' ||
-                nextChar != '7' || nextChar != '8' || nextChar != '9')
-                {
-                    printf("Invalid character\n");
-                    break;
-                }
-            if (nextChar == currentChar){
+    int newLength=0;
+    const int ascissoffset=48;
+    int index=0;
+    char current;
+    char prev;
+    char* tmp=malloc(2);
+    char* new=malloc(1);
+    new[0]='\0';
+    while (old[index]!='\0'){
+        index++;
+        if (index>0){
+            current=old[index];
+            prev=old[index-1];
+            if (current==prev){
                 count++;
             }
             else {
-                //retString[lenNextString]=malloc(sizeof(char));
-                //retString[lenNextString+1]=malloc(sizeof(char));
-
-                newString[lenNextString]=count+asciioffset;            
-                newString[lenNextString+1]=currentChar;
-
-                currentChar=nextChar;
-                lenNextString+=2;
+                newLength+=2;
+                new=realloc(new,newLength+1);
+                tmp[0]=count+ascissoffset;
+                tmp[1]='\0';
+                strcat(new,tmp);
+                tmp[0]=old[index-1];
+                tmp[1]='\0';
+                strcat(new,tmp);
+                count=1;
             }
         }
     }
-    return lenNextString;
-} 
+    return new;
+}
 
-int main(void){
-    
-    char input[] = {'1'};
-    char nextString[] = {'\0'};
-    const char nullString[] = {'\0'};
-    int length = 1;
-    int nextLength = 0;
-    int ncycles=2;
+void main(void){
 
-    for (int nrepeats=0; nrepeats<ncycles; nrepeats++)
-    {
-        printf("repeat count %d of %d\n",nrepeats,ncycles);
-        nextLength = newString(input, nextString, length);
+    char* nums="1113122113"; // 132113 3113 1113 13 - Conway Sequence
+    const int nrepeats=40; // Part 1
+    // const int nrepeats=50; // Part 2 - works but takes about an hour
 
-        for (int i=0; i<length; i++) {printf("%c", input[i]);}
-        printf("\n");
-        printf("Length of current %d", length);
-        printf("\n");
-        for (int i=0; i<nextLength; i++) {printf("%c", nextString[i]);}
-        printf("\n");
-        printf("Length of next %d", nextLength);
-        printf("\n");
-
-        length=nextLength;
-        memcpy(input, nextString, length);
-        memcpy(nextString, nullString, 1);
+    for (int i=0; i<nrepeats; i++){
+        nums=newString(nums);
+        printf("%d of %d\n",i,nrepeats);
     }
 
-
-
-    
-   
-    
-    return 0;
+    printf("\n");
+    printf("Solution is %d\n", strlen(nums));
 }
