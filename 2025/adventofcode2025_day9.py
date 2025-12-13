7d234dd360ddd10805457335bf0b066eb10fcae9
troutnodoubt <truta11@yahoo.com>
2025-12-13 15:49:47 -0600

    day 9 working slow

--- 2025/adventofcode2025_day9.py
+++ 2025/adventofcode2025_day9.py
@@ -69,59 +69,12 @@ def point_on_polygon_edge(polygon, point):
         j=i
     return onedge
         
-
-
-# def point_in_polygon(polygon, point):
-#     num_vertices = len(polygon)
-#     x, y = point[0], point[1]
-#     inside = False
-
-#     # Store the first point in the polygon and initialize the second point
-#     p1 = polygon[0]
-
-#     # Loop through each edge in the polygon
-#     for i in range(1, num_vertices + 1):
-#         # Get the next point in the polygon
-#         p2 = polygon[i % num_vertices]
-
-#         # Check if the point is above the minimum y coordinate of the edge
-#         if y > min(p1[1], p2[1]):
-#             # Check if the point is below the maximum y coordinate of the edge
-#             if y <= max(p1[1], p2[1]):
-#                 # Check if the point is to the left of the maximum x coordinate of the edge
-#                 if x <= max(p1[0], p2[0]):
-#                     # Calculate the x-intersection of the line connecting the point to the edge
-#                     x_intersection = (y - p1[1]) * (p2[0] - p1[0]) / (p2[1] - p1[1]) + p1[0]
-
-#                     # Check if the point is on the same line as the edge or to the left of the x-intersection
-#                     if p1[0] == p2[0] or x <= x_intersection:
-#                         # Flip the inside flag
-#                         inside = not inside
-
-#         # Store the current point as the first point for the next iteration
-#         p1 = p2
-
-#     # Return the value of the inside flag
-#     return inside
-
 biggesta=0
 biggestgreen=0
 polygon=[]
 for point1 in data:
     polygon.append((int(point1.split(',')[0]),int(point1.split(',')[1])))
-# print("pointtests")
-# print(point_in_polygon(polygon,(7,1)))
-# print(point_in_polygon(polygon,(8,1)))
-# print(point_in_polygon(polygon,(9,1)))
-# print(point_in_polygon(polygon,(10,1)))
-# print(point_in_polygon(polygon,(11,1)))
-# print(point_in_polygon(polygon,(12,1)))
-# print(point_in_polygon(polygon,(9,2)))
-# print(point_in_polygon(polygon,(11,7)))
-# print(point_in_polygon(polygon,(3,3)))
-# print(point_in_polygon(polygon,(3,4)))
-# print(point_in_polygon(polygon,(8,3)))
-# print()
+
 for i,point1 in enumerate(data):
      print(i+1," of ",len(data))
      for point2 in data:
@@ -129,23 +82,15 @@ for i,point1 in enumerate(data):
             p1=(int(point1.split(',')[0]),int(point1.split(',')[1]))
             p2=(int(point2.split(',')[0]),int(point2.split(',')[1]))
             a=area(p1,p2)
-            # print(p1,p2,oppositeCorners(p1,p2))
+
             testedges=perimeter(p1,p2)
             isgreen=True
             for edgepoint in testedges:
-            # for edgepoint in oppositeCorners(p1,p2):
-                # print(edgepoint,point_in_polygon(polygon,edgepoint))
-                # print(not point_in_polygon(polygon,edgepoint) and not (edgepoint != p1 and edgepoint != p2))
                 if not point_in_polygon(polygon,edgepoint) and not point_on_polygon_edge(polygon,edgepoint):
-                    
-                    # print("not green",p1,p2)
                     isgreen=False
                     break
             if isgreen:
-                # print("green",p1,p2,a)
-                if a>biggestgreen: biggestgreen=a
-            # else: break   
-            
+                if a>biggestgreen: biggestgreen=a            
             
             if a>biggesta: biggesta=a
 
@@ -154,6 +99,8 @@ print("Part 2 solution ",biggestgreen)
 
 # part 2:
 
-# define perimeter of test area
-# if any point on perimeter not in red/green area, move to the next test area
-# check if points are in red green area with raytracing algorithm, see 2023 day 10
+'''
+This works and gives the right answer, but is highly inefficient. Would have been better to check
+for intersecting lines to determine if a square was enclosed by the polygon rather than
+every point, even with early exit on the point checks.
+'''
