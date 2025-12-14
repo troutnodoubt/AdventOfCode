f2dfe1603777f2ef9e4a7b0ea887a4c304b05570
troutnodoubt <truta11@yahoo.com>
2025-12-13 21:01:25 -0600

    day 7 pt 2

--- 2025/adventofcode2025_day7.py
+++ 2025/adventofcode2025_day7.py
@@ -1,22 +1,6 @@
 
 fname='/home/mark/Documents/git/AdventOfCode/2025/input_day7.txt'
-fname='/home/mark/Documents/git/AdventOfCode/2025/example_day7.txt'
-
-branches=dict()
-
-def hasSplittingDescendants(node):
-    if node in branches.keys():
-        children=branches[node]
-        # print(node,branches[node],children)
-        
-           
-        if len(children)>1: return True
-        elif len(children)==1 and hasSplittingDescendants(children[0]): return True
-        else: return False
-
-    else:
-        return False
-
+# fname='/home/mark/Documents/git/AdventOfCode/2025/example_day7.txt'
 
 with open(fname) as fp: data1 = fp.read().splitlines()
 
@@ -30,78 +14,41 @@ splits=dict()
 start=0
 rowlength=len(data[0])
 for row in range(len(data)-1):
-    for col in range(1,len(data[0])):
+    for col in range(0,len(data[0])):
         if data[row][col]=='S':
             data[row+1][col]='|'
             start=(row+1)*rowlength+col
         elif data[row][col]=='|':
             if data[row+1][col]=='.':
                 data[row+1][col]='|'
-                branches[row*rowlength+col]=[(row+1)*rowlength+col]
             elif data[row+1][col]=='^':
                 data[row+1][col-1]='|'
                 data[row+1][col+1]='|'
                 splitcnt+=1
-                branches[row*rowlength+col]=[(row+1)*rowlength+col]
-                branches[(row+1)*rowlength+col]=[(row+1)*rowlength+col-1,(row+1)*rowlength+col+1]
-finalrow=len(data)-1
-for col in range(1,rowlength):
-    if data[finalrow][col]=='|':
-        branches[finalrow*rowlength+col]=[]
 
-# for key in branches.keys():
-    # print(key)
-    # if len(branches[key])>1: 
-        # nsplits+=1
-        # print('haschildren',key)
-
-for key in branches.keys():
-    print(key,branches[key])
-print()
-
-for row in range(len(data),0,-1):
-    for col in range(rowlength):
-        node=row*rowlength+col
-        count=0
-        if node in branches.keys() and len(branches[node])>1: #splitting node
-            # print(node,row,len(data))
-            if row==len(data)-2: # final row of splitters
-                # print('final row',node)
-                splits[node]=2
+nworlds=[]
+nrow=0
+sol=0
+for row in range(len(data)-1,0,-1):
+    temp=[]
+    for col in range(len(data[0])):
+        if row==len(data)-1:
+            if data[row][col]=='|':
+                temp.append(1)
             else:
-                for child in branches[node]:
-                    splits[node]=0
-                    
-                    # print(node,branches[node],child)
-                    print(child,hasSplittingDescendants(child))
-                    print(child in splits.keys()) # on the right track, but keys to the right haven't processed yet
+                temp.append(0)
+        else:
+            if data[row][col]=='|':
+                temp.append(nworlds[nrow-1][col])
+            else:
+                temp.append(0)
+               
+    nworlds.append(temp)
+    for col in range(len(data[0])):
+        if data[row][col]=='^':
+            temp[col]=temp[col-1]+temp[col+1]
+            if temp[col]>sol: sol=temp[col]
+    nrow+=1
 
-# Need to do this in steps:
-# 1. Populate last row (done)
-# 2. Propagate values up to next row
-# 3. Populate splitting cells
-# 4. Repeat 2 and 4 until done
-
-
-
-
-                    if hasSplittingDescendants(child) and child in splits.keys():
-                        count+=splits[child]
-                        print('has splitters',node,count)
-                    else:
-                        count+=1
-                        print('no more splitters',node,count)
-                    splits[node]+=count
-                    print(node,splits[node])
-        else: # non splitting node, just propagate the count up the chain
-            if node in branches.keys() and len(branches[node])>0 and branches[node][0] in splits.keys(): 
-                # print(branches[node])
-                splits[node]=splits[branches[node][0]]
-
-print()
-for key in splits.keys():
-    print(key,splits[key])            
-print()
 print('Part 1 is',splitcnt)
-# print(branches)
-
+print('Part 2 is',sol)
