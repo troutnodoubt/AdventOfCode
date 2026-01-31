b692c8bedd06c07df38990924003233bd9af73b2
troutnodoubt <truta11@yahoo.com>
2026-01-31 15:40:59 -0600

    day11

--- 2025/adventofcode2025_day11.py
+++ 2025/adventofcode2025_day11.py
@@ -1,42 +1,55 @@
-fname='2025\\input_day11.txt'
-# fname='2025\\example_day11.txt'
-# fname='2025\\example_day11pt2.txt'
+fname='2025/input_day11.txt'
+# fname='2025/example_day11.txt'
+# fname='2025/example_day11pt2.txt'
 
 with open(fname) as fp: data = fp.read().splitlines()
 
-visited=set() 
-
-def DFS(connections,node,allpaths,path,dest):
-    if node in path: print("going in circles")
-    path.append(node)
-    # print(node)
+def DFS(connections,node,dest,memo):
     if node==dest:
-        allpaths.append(path.copy())
-    else:
-        for neighbor in connections[node]:
-            DFS(connections,neighbor,allpaths,path,dest)
-    path.pop()
+        return 1
+    if memo[node]!=-1:
+        return memo[node]
+    count=0
+    for neighbor in connections[node]:
+        count+=DFS(connections,neighbor,dest,memo)
+    memo[node]=count
+    return count
 
-connections=dict()
+connections={}
+memo={}
 for row in data:
-    connections[row.split()[0][0:-1]]=row.split()[1:]
+    connections[row.split()[0][0:-1]]=set(row.split()[1:])
 
-allpaths=[]
-path=[]
-connections['out']=[]        
-# DFS(connections,'you',allpaths,path,'out')
+for key in connections.keys():
+    memo[key]=-1
 
-# # print(allpaths)
-# print('Part 1',len(allpaths))
+connections['out']=()
+for key in connections.keys():
+    memo[key]=-1
+count=DFS(connections,'you','out',memo)
 
-allpaths=[]
-path=[]
-DFS(connections,'svr',allpaths,path,'out')
+print('Part 1',count)
 
-count=0
+for key in connections.keys():
+    memo[key]=-1
+counta1=DFS(connections,'svr','fft',memo)
+for key in connections.keys():
+    memo[key]=-1
+counta2=DFS(connections,'fft','dac',memo)
+for key in connections.keys():
+    memo[key]=-1
+counta3=DFS(connections,'dac','out',memo)
 
-for path in allpaths:
-    print(path)
-    if 'fft' in path and 'dac' in path: count+=1
+for key in connections.keys():
+    memo[key]=-1
+countb1=DFS(connections,'svr','dac',memo)
+for key in connections.keys():
+    memo[key]=-1
+countb2=DFS(connections,'dac','fft',memo)
+for key in connections.keys():
+    memo[key]=-1
+countb3=DFS(connections,'fft','out',memo)
+
+count=counta1*counta2*counta3+countb1*countb2*countb3
 
 print('Part 2', count)
